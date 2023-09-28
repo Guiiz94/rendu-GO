cc865c202a94f5226b9ea6bbdff85e6298059341
Matteo <mrodrigues7@myges.fr>
2023-09-28 16:08:40 +0200

    .fix add token api github to the request

--- adapter/repositoryLister.go
+++ adapter/repositoryLister.go
@@ -5,18 +5,43 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"os"
+	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/Guiiz94/rendu-GO/domain"
 )
 
 type GithubAPIAdapter struct{}
 
 func (g *GithubAPIAdapter) List(username string) ([]domain.Repository, error) {
-	resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/repos", username))
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("GITHUB_TOKEN")
+	
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.github.com/users/%s/repos", username), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Si le token est disponible, on l'ajoute à la requête
+	if token != "" {
+		req.Header.Set("Authorization", "token "+token)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("GitHub API returned non-OK status: %v", resp.Status)
+	}
 
 	var apiRepos []struct {
 		Name       string    `json:"name"`
